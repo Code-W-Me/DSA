2b3f84ac7b0fcea1ee6e0670be72889b7470d0b2
yashveer <yashveer7264@gmail.com>
2025-11-01 13:40:04 +0530

    bits concepts and problems

--- bitManipulation.java
+++ bitManipulation.java
@@ -19,6 +19,7 @@ public static int getIthBit(int n , int i){
         return 1;
     }
 }
+// Set ith bit
 public static int setIthBit(int n, int i){
     int bitMask = 1<<i;
     return n | bitMask;
@@ -27,13 +28,56 @@ public static int clearIthBit(int n , int i){
     int bitMask = ~(1<<i);
     return n & bitMask;
 }
+public  static int clearIBits( int n, int i){
+    int bitMask = ~(0)<<i;
+    return n & bitMask;
+}
+
+public static int clearBitsinRange(int n , int i , int j){
+    int a = ((-0)<<j+1);
+    int b = (1<<i)-1;
+    int bitMask = a | b;
+    return n & bitMask;
+}
+
+// Question : Check if a number is a power of 2
+public static boolean isPowerOfTwo(int n){
+    return (n & (n-1)) == 0;
+}
+public static int countSetBits(int n){
+    int count = 0;
+    while(n>0){
+        if((n & 1) !=0){
+            count++;
+        }
+        n = n>>1;
+    }
+    return count;
+}
+public static int fastExpo(int a , int n){
+    int ans = 1;
+    while(n>0){
+        if((n & 1) != 0){
+            ans = ans * a;
+        }
+        a = a * a;
+        n = n >> 1;
+    }
+    return ans;
+}
+
 public static void main(String args[]){
     // oddOrEven(7);
     // oddOrEven(4);
     // oddOrEven(0);
     // System.out.println(getIthBit(5,0));
     // System.out.println(setIthBit(10,2));
-    System.out.println(clearIthBit(10,1));
+    // System.out.println(clearIthBit(10,1));
+    // System.out.println(clearIBits(15,2));
+    // System.out.println(clearBitsinRange(10,2,4));
+    // System.out.println(isPowerOfTwo(15));
+    // System.out.println(countSetBits(2));
+    System.out.println(fastExpo(2,6));
 
   }
 }
