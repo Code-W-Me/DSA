c2ce4635223a8aafe8e7ea3347fc8dbb4f07803c
yashveer <yashveer7264@gmail.com>
2025-11-08 22:05:55 +0530

    RECURSION PT 2

--- Recursion.java
+++ Recursion.java
@@ -28,9 +28,34 @@ public class Recursion {
         int fn = n*fnm1;
         return fn;
     }
+    public static int Sum(int n){
+        if(n==1){
+            return 1;
+        }
+        int sumnm1 = Sum(n-1);
+        int sum = n+sumnm1;
+        return sum;
+    }
+
+    public static int fibonacci(int n ){
+        if(n==0 || n==1){
+            return n;
+        }
+        int fnm1 = fibonacci(n-1);
+        int fnm2 = fibonacci(n-2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
+
+
+
+
+
     public static void main(String[] args){
     int n = 10;
-    System.out.println("factorial would be:" + factorial(5));
+    // System.out.println("factorial would be:" + factorial(5));
+    // System.out.println("Sum would be:" + Sum(5));
+    System.out.println("Fibonacci would be:" + fibonacci(5));   
 }
 }
 
