5fb9dc289ec24daaac1874a6cf06ce652ab0ebd4
yashveer <yashveer7264@gmail.com>
2025-11-20 14:14:16 +0530

    sorting the arr with the help of pivot

--- divideNConquer.java
+++ divideNConquer.java
@@ -49,9 +49,43 @@ public class divideNConquer {
 }
 
 
+public static void quickSort(int arr[],int si, int ei){
+    //base case
+    if(si>=ei){
+        return;
+    }
+    //kaam
+    int pIdx = partition(arr,si,ei);
+    quickSort(arr,si,pIdx-1); // left part
+    quickSort(arr, pIdx+1, ei); // right part
+}
+public static int partition(int arr[], int si, int ei){
+    int pivot = arr[ei];
+    int i = si-1; //to make place for smaller element than pivot
+    for(int j=si; j<ei; j++){
+        if(arr[j]<=pivot){
+            i++;
+            //swap
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+
+}
+
+
     public static void main(String args[]){
-        int arr[] = {6,3,9,5,2,8};
-        mergeSort(arr,0,arr.length-1);
+        // int arr[] = {6,3,9,5,2,8,-2};
+        // mergeSort(arr,0,arr.length-1);
+        // printArr(arr);
+        int arr[] = {6,3,9,8,2,5};
+        quickSort(arr,0,arr.length-1);
         printArr(arr);
     }
 }
