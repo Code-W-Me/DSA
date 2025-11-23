36fb9e1821d7fb5216cb1ae2d0e50765aeeb696a
yashveer <yashveer7264@gmail.com>
2025-11-23 15:40:36 +0530

    solved some prblems for practice

--- divideNConquer.java
+++ divideNConquer.java
@@ -78,14 +78,110 @@ public static int partition(int arr[], int si, int ei){
         return i;
 
 }
+public static int search(int arr[],int tar, int si, int ei){
+    //work
+    int mid = (si+ei)/2;
+    //case FOUND
+    if(arr[mid]==tar){
+        return mid;
+    }
+    // mid on L1
+    if(arr[si]<=arr[mid]){
+        //case a: left
+        if(arr[si]<=tar && tar<arr[mid]){
+            return search(arr, tar, si, mid-1);
+        }else{ //case b: right
+            return search(arr,tar,mid+1,ei);
+        }
+    }
+    // mid on L2
+    else{
+        //case a:right
+        if(arr[mid]<tar && tar<=arr[ei]){
+            return search(arr, tar, mid+1, ei);
+        }else{ //case b: left
+            return search(arr, tar, si, mid-1); 
+        }
+    }
+}
+
+public static String[] mergeSort(String[] arr, int lo , int hi){
+    //base case
+    if(lo==hi){
+        String[] A = {arr[lo]};
+        return A;
+    }
+
+    //kaam
+    int mid = (lo+hi)/2;
+    String[] str1 = mergeSort(arr,lo,mid);
+    String[] str2 = mergeSort(arr,mid+1,hi);
+    String[] str3 = merge(str1,str2);
+    return str3;
+}
+ static String[] merge(String[] str1, String[] str2){
+    int m = str1.length;
+    int n = str2.length;
+    String[] str3 = new String[m+n];
+    int idx = 0;
+    int i = 0;
+    int j=0;
+
+    while(i<m && j<n){
+        if(isAlphabeticallySmaller(str1[i],str2[j])){
+            str3[idx]=str1[i];
+            i++;
+            idx++;
+        }else{
+            str3[idx]=str2[j];
+            j++;
+            idx++;
+        }
+    }
+    while(i<m){
+        str3[idx]=str1[i];
+        i++;
+        idx++;
+    }
+    while(j<n){
+        str3[idx]=str2[j];
+        j++;
+        idx++;
+    }
+    return str3;
+    }
+    
+
+public static boolean isAlphabeticallySmaller(String str1, String str2){
+    if(str1.compareTo(str2)<0){
+        return true;
+    }
+    return false;
+}
+    
+
 
 
     public static void main(String args[]){
         // int arr[] = {6,3,9,5,2,8,-2};
         // mergeSort(arr,0,arr.length-1);
         // printArr(arr);
-        int arr[] = {6,3,9,8,2,5};
-        quickSort(arr,0,arr.length-1);
-        printArr(arr);
-    }
-}
+        // int arr[] = {4,5,6,7,0,1,2};
+        // quickSort(arr,0,arr.length-1);
+        // printArr(arr);
+        // int arr[] = {4,5,6,7,0,1,2};
+        // int target = 0;
+        // int tarIdx = search(arr, target, 0, arr.length-1);
+        // System.out.println(tarIdx);
+
+        // String str = mergeSort(new String[]{"sun","earth","mars","mercury"}, 0, 3);
+        // System.out.println(str);
+        String[] str = {"sun","earth","mars","mercury"};
+        String[] a = mergeSort(str,0,str.length-1);
+        for(int i =0; i<a.length;i++){
+            System.out.print(a[i]+" ");
+
+        }
+        System.out.println();
+    } 
+}   
