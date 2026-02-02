9861f6989d73979773ef7fccfbd43fa82fdb36ce
yashveer <yashveer7264@gmail.com>
2026-02-02 15:39:33 +0530

    backtracking in permutaion

---
+++ Permutaion.java
@@ -0,0 +1,23 @@
+import java.util.*;
+ public class Permutation {
+
+    public static void findPermuation( String str , String ans){
+        //base case
+        if(str.length() ==0){
+            System.out.println(ans);
+            return; 
+        }
+        //recurion
+        for(int i = 0; i<str.length();i++){
+            char curr = str.charAt(i);
+            // to remove ith character from str
+            // Example: "abcde" => "ab" + "de"  = "abde"
+            String NewStr = str.substring(0,i) + str.substring(i+1);
+            findPermuation(NewStr, ans + curr);
+        }
+    }
+public static void main(String args[]){
+    string str = "abc";
+    findPermuation(str, "");
+  }
+}
